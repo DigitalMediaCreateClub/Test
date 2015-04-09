44905bef85eca2ce997f8ef56a00a6510869fe73
Tommy <jeatommy@gmail.com>
2015-04-10 00:11:22 +0900

    コメント削除

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  Test
-//
-//  Created by 富岡 稔 on 2015/04/10.
-//  Copyright (c) 2015年 富岡 稔. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[])
