470a317a1253766416f0e310947418fe18aed1d6
Tommy <jeatommy@gmail.com>
2015-04-10 00:09:50 +0900

    initial commit

---
+++ Test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by 富岡 稔 on 2015/04/10.
+//  Copyright (c) 2015年 富岡 稔. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
